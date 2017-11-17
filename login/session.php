20cc062df190b894cbd68df331955bc43a681780
MiniMiudo <mmtb4ever@gmail.com>
2017-11-17 12:35:28 +0000

    Create session.php

---
+++ login/session.php
@@ -0,0 +1,26 @@
+<?php
+    include_once("/usr/users2/mieec2013/up201305659/public_html/SEAI/database/database.php");
+
+    /* Permite bloquear páginas com acesso restrito a utilizadores com login efectuado - para já não está a ser usado */
+    function check_session ($path) {
+        if ($_SESSION["user_id"] == NULL){
+            header("Location: $path");
+            die();
+        }
+    }
+    
+	/* Permite bloquear páginas com acesso restrito a administradores - para já não está a ser usado */
+    function check_admin ($path) {
+            db();
+
+        if (!check_admin_db()){
+            header("Location: $path");
+            die();
+        }
+    }
+
+    session_start();
+    if (!isset($_SESSION["user_id"])){
+        $_SESSION["user_id"] = NULL;
+    }
+?>
