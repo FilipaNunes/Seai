8c2b61e3c6802c295822603f89a2c5a52d798a6d
FilipaNunes <filipanunes12@hotmail.com>
2017-11-17 11:16:36 +0000

    SQL Injection Avoid

--- database/database.php
+++ database/database.php
@@ -2,29 +2,36 @@
   function db(){
     global $conn;
     if(!isset($conn)){
-      $conn = pg_connect("host=db.fe.up.pt dbname=ee12299 user=ee12299 password=drone");
+		$conn = new PDO('pgsql:host=db.fe.up.pt;dbname=ee12299', 'ee12299', 'drone');
+		$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } else {
       return;
     }
     if(!$conn) {
-      echo "An error ocurred.\n";
-      exit;
+      die ("An error ocurred!");
     }
 //debug
-error_log("\ndbg:Connected Successfully to DataBase!\n");
-    $query = "set schema 'ola';";
-    pg_exec($conn, $query);
+//echo("\ndbg:Connected Successfully to DataBase!\n");
+
+    $schema = $conn->exec("SET SCHEMA 'ola';");
   }
 ?>
 
 
 <?php
-function execQuery($query){
+function execQuery($query,$insert, $array){
   db();
-  global $conn;
+global $conn;
 //debug
 error_log("\ndbg:SQL-Query: " . $query . " --end--");
-  $result = pg_exec($conn, $query);
+
+  $result = $conn->prepare($query);
+  
+  for($i=0;$i<sizeof($array);$i++){
+	  $result->bindParam($insert[$i], $array[$i]);
+	}
+  $result->execute();
   return $result;
 }
 ?>
--- registo/check.php
+++ registo/check.php
@@ -4,13 +4,18 @@
 <?php
 
 	function addUser($login,$password,$email){
-		$query = "INSERT INTO clientes(username,password,email) VALUES('" . $login . "','" . md5($password) . "','" . $email . "');";
-		$result = execQuery($query);
+		$query = "INSERT INTO clientes(username,password,email)
+			      VALUES(:name, :pass, :email)";
+		
+		$values = array($login,(md5($password)),$email);
+		$insert = array(':name',':pass', ':email');
+		
+		$result = execQuery($query,$insert,$values);
 	}
 
 	function CheckUser($user){
 	$query = "SELECT username FROM clientes WHERE username = '".$user."'";
-	$result = execQuery($query);
+	$result = execQuery($query,$array($user));
 	$num_registos = pg_numrows($result);
 	
 	return $num_registos;
