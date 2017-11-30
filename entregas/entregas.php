3f39e750ab951b8f61dfe20d3cd6429565db06b1
MiniMiudo <mmtb4ever@gmail.com>
2017-11-30 19:27:34 +0000

    Update entregas.php

--- entregas/entregas.php
+++ entregas/entregas.php
@@ -1,3 +1,5 @@
+<?php include_once('pagination.php'); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,9 +8,17 @@
 </head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/notificacao.css"> 
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 <link rel="stylesheet" href="../css/style.css">
+
+<script src='entregas.js' charset='utf-8'></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.0.3/sweetalert2.all.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+
 <style>
 body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
 body {font-size:16px;}
@@ -42,7 +52,7 @@ body {font-size:16px;}
 </header>
 
 <!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="Fechar Menu" id="myOverlay"></div>
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
 
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:340px;margin-right:40px">
@@ -50,66 +60,34 @@ body {font-size:16px;}
 	<div class="w3-container" id="packages" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-red"><b>Entregas</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
-  </div>
+	</div>
 
      <?php 
-        /* $user_data = user_data_db(); <- função que obtem dados do utilizador autenticado
-        $user_data = pg_fetch_assoc($user_data); */
+		include_once("../database/database.php");
+		include_once("../login/session.php");
+		
     ?> 
-				<div id="entregas1" class="entregas">
 				<div class="w3-container">
-				<div style="float: right">Estado: <div class="styled-select"><Select name="estado">
-				<Option value="pendente">Pendente</Option>
-				<Option value="pendente">Em processamento</Option>
-				<Option value="pendente">Expedida</Option>
-				<Option value="pendente">Entregue</Option>
-				</select></div></div><br>
 					<table class="w3-table-all">
 					  <thead>
 						<tr class="w3-red">
-						  <th>Cliente</th>
-						  <th>Serviço</th>
-						  <th>Produto</th>
-						  <th>Custo</th>
-						  <th>Envio</th>
-						  <th>Entrega</th>
-						  <th>Estado</th>
+						  <th style='text-align:center'>Cliente</th>
+						  <th style='text-align:center'>Produto</th>
+						  <th style='text-align:center'>Custo</th>
+						  <th style='text-align:center'>Destino</th>
+						  <th style='text-align:center'>Envio</th>
+						  <th style='text-align:center'>Entrega</th>
+						  <th style='text-align:center'>Estado</th>
+						  <th style='text-align:center'></th>
 						</tr>
 					  </thead>
-					  <tr>
-						<td>Lorem Ipsum</td>
-						<td>0 a 1 Kg</td>
-						<td>Livro</td>
-						<td>19,99 €</td>
-						<td>04/11/2017 14:02</td>
-						<td>04/11/2017 14:15</td>
-						<td>Entregue</td>
-					  </tr>
-					  <tr>
-						<td>Lorem Ipsum</td>
-						<td>2 a 3 Kg</td>
-						<td>Telemovel</td>
-						<td>39,99 €</td>
-						<td>04/11/2017 14:05</td>
-						<td>04/11/2017 14:18</td>
-						<td>Entregue</td>
-					  </tr>
-					  <tr>
-						<td>Lorem Ipsum</td>
-						<td>1 a 2 Kg</td>
-						<td>Relógio</td>
-						<td>29,99 €</td>
-						<td>08/11/2017 17:01</td>
-						<td>08/11/2017 17:20</td>
-						<td>Entregue</td>
-					  </tr>
-					</table>
+					  <?php Table(); ?>
 				  </div>
 				</div>
 
   <!-- End page content -->
   </div>
-
+  
   <script>
   // Script to open and close sidebar
   function w3_open() {
@@ -124,10 +102,11 @@ body {font-size:16px;}
 
   </script>
   
-   <script>
+  
+<script>
   function openTab(evt, tabName) {
     var i, x, tablinks;
-    x = document.getElementsByClassName("entregas");
+    x = document.getElementsByClassName("dados");
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
@@ -137,13 +116,19 @@ body {font-size:16px;}
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.firstElementChild.className += " w3-border-red";
+	
   }
-</script>
+</script> 
 
 <script>
   var mybtn = document.getElementById("testbtn");
   mybtn.click();
 </script>
+
+<script>
+   	document.getElementById("defaultOpen").click();
+</script>
+
 
   </body>
   </html>
