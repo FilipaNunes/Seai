161e4371649f3e919c4684c20ad1e8400861ae66
MiniMiudo <mmtb4ever@gmail.com>
2017-12-05 16:17:20 +0000

    Update action_update.php

--- dados_pessoais/action_update.php
+++ dados_pessoais/action_update.php
@@ -26,19 +26,31 @@
 	
 	$password_md5 = md5($_POST["password"]);
 	
+	$query = "SELECT nif FROM clientes WHERE nif = :nif";
+	$values = array($_POST["nif"]);
+	$insert = array(':nif');
+	$result = execQuery($query, $insert, $values);
+	$result2 = $result->rowCount($result);
+	
 	if ($_POST["update"] AND ($password_md5 != $user_data1["password"])) {
 		header("Location: update_dados2.php");
 		exit();
 	}
 	
 	elseif ($_POST["update"] AND $_POST["new_password"]==NULL) {
+		if ($result2 > 0) header("Location: update_dados3.php");
+		else {
 		update_user_db($_POST["name"], $_POST["email"], $_POST["username"], $password_md5, $_POST["telephone"], $_POST["nif"], $_POST["address"]);
 		header("Location: dados_pessoais.php");
+		}
 	}
 
 	elseif ($_POST["update"] AND $_POST["new_password"]!=NULL) {
+		if ($result2 > 0) header("Location: update_dados3.php");
+		else {
 		$new_password_md5 = md5($_POST["new_password"]);
 		update_user_db($_POST["name"], $_POST["email"], $_POST["username"], $new_password_md5, $_POST["telephone"], $_POST["nif"], $_POST["address"]);
 		header("Location: dados_pessoais.php");
+		}
 	}
 ?>
