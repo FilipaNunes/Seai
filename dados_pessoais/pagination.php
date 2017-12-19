222bafe625ec4e9eb9cea7f32c91c642756fccf2
MiniMiudo <mmtb4ever@gmail.com>
2017-12-19 14:01:01 +0000

    Update pagination.php

--- dados_pessoais/pagination.php
+++ dados_pessoais/pagination.php
@@ -82,7 +82,7 @@
 			$insert = array(':id_recolha');
 
 			$destino3_temp = execQuery($query,$insert,$values);
-			$destino3 = $destino2_temp->fetch();
+			$destino3 = $destino3_temp->fetch();
 			?>
 
 						<tr style='text-align:center'>
