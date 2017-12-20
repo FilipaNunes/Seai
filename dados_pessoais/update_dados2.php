d6806e714e62a3626e4de30398f8a03524cedef4
MiniMiudo <mmtb4ever@gmail.com>
2017-12-20 11:49:30 +0000

    Nova password entre 6 e 20 caracteres

--- dados_pessoais/update_dados2.php
+++ dados_pessoais/update_dados2.php
@@ -35,7 +35,7 @@ body {font-size:16px;}
 		<a href="../contactos/index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-red">Contactos</a>
 		<a href="../colaboradores/index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-red">Colaboradores</a>
   <?php if ($_SESSION["user_id"] == NULL) { ?>
-		<hr style="border-width: 2px; border-color: red">
+		<hr style="border-width: 2px; border-color: red"> 
 		<a href="../login/index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-red">Login</a>
   <?php } 
 		elseif ($_SESSION["user_id"] != NULL AND check_admin_db() == 0) { ?>
